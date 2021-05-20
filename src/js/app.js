819ce83f14abf34956da887038333373828ee148
DebjitPramanick <dpramanick9990@gmail.com>
2021-05-20 10:09:18 +0530

    Candidate Cards

--- src/js/app.js
+++ src/js/app.js
@@ -18,30 +18,8 @@ App = {
     return App.initContract();
   },
 
-  // init: async function() {
-  //   // Load pets.
-  //   $.getJSON('../pets.json', function(data) {
-  //     var petsRow = $('#petsRow');
-  //     var petTemplate = $('#petTemplate');
-
-  //     for (i = 0; i < data.length; i ++) {
-  //       petTemplate.find('.panel-title').text(data[i].name);
-  //       petTemplate.find('img').attr('src', data[i].picture);
-  //       petTemplate.find('.pet-breed').text(data[i].breed);
-  //       petTemplate.find('.pet-age').text(data[i].age);
-  //       petTemplate.find('.pet-location').text(data[i].location);
-  //       petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-  //       petsRow.append(petTemplate.html());
-  //     }
-  //   });
-
-  //   return await App.initWeb3();
-  // },
-
   initContract: async function () {
     const election = await(await fetch('Election.json')).text()
-    console.log(JSON.parse(election))
     App.contracts.Election = TruffleContract(JSON.parse(election))
     App.contracts.Election.setProvider(App.web3Provider)
     App.election = await App.contracts.Election.deployed()
@@ -55,7 +33,6 @@ App = {
       electionInstance = instance
       return electionInstance.candidateCounts()
     }).then(function (candidateCounts) {
-      // var candidatesResults = document.getElementById('candidateResults')
 
       var candidatesRow = document.getElementById('candidatesRow')
       var candidateTemplate = document.getElementById('candidateTemplate')
@@ -66,15 +43,18 @@ App = {
           var name = candidate[1];
           var votes = candidate[2];
 
-          let title = document.getElementById('panel-title')
-          title.innerText = name
-          // candidateTemplate.childNodes.item('.cand-ID').innerText(id)
-          // candidateTemplate.childNodes.item('.cand-name').innerText(name)
-          // candidateTemplate.childNodes.item('.cand-votes').innerText(votes)
+          let cndTmplt = candidateTemplate.cloneNode(true)
 
-          candidatesRow.append(candidateTemplate);
+          let fields = cndTmplt.querySelectorAll(".field")
+          fields[0].innerHTML = name
+          fields[1].innerHTML = id
+          fields[2].innerHTML = name
+          fields[3].innerHTML = votes
+          console.log(cndTmplt)
 
+          cndTmplt.style.display="block"
 
+          candidatesRow.append(cndTmplt);
         })
       }
     })
